26f3b75843b08dc7dcfc97b25d98164001c5b2b5
lailahgrant <lailahgrant19@gmail.com>
2025-07-03 18:35:31 +0300

    setting up the HTML

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google Calendar Clone Project</title>
+    <meta name="description" content="A simple Google Calendar clone project built with PHP, MySQL, HTML, CSS, and JavaScript.">
+</head>
+<body>
+    
+</body>
+</html>
